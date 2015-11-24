cc1c48478a19d8f34e302a2e369805ff15f287f9
Sergey Bespalov <darkclarus@gmail.com>
2015-11-24 21:22:34 +0200

    add nconfig and winston, add 5 methods for crud https://chisw.com.ua/redmine/issues/6861

--- app.js
+++ app.js
@@ -3,27 +3,42 @@
  */
 var express = require("express");
 var http    = require("http");
-var url     = require("url");
-var path    = require("path");
+var config  = require("./config");
+var log     = require("./libs/log")(module);
+
 
 var app = express();
 
-app.set("port", 3000);
+app.set("port", config.get("port"));
 
 http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port: " + app.get('port'));
+    log.info("Express server listening on port: " + config.get("port"));
 });
 
+app.get('/', function (req, res) {
+    res.status(200).send('Index page!');
+});
 
-app.use(function(req, res, next) {
-    if(req.url == '/') {
-        res.end("Hello");
-    } else {
-        next();
-    }
+app.get('/topic', function (req, res) {
+    res.status(200).send('Get all topics');
+});
 
+app.get('/topic/:id', function(req, res){
+    res.status(200).send('Get topic ' + req.params.id);
+});
+
+app.post('/topic', function (req, res) {
+    res.status(201).send('Create topic');
+});
+
+app.put('/topic/:id', function (req, res) {
+    res.status(200).send('Update topic ' + req.params.id);
+});
+
+app.delete('/topic/:id', function (req, res) {
+    res.status(200).send('Delete topic ' + req.params.id);
 });
 
 app.use(function(req, res) {
-    res.send(404, "Page not found, sorry.");
+    res.status(404).send("Page not found, sorry.");
 });
