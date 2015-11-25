bc30265b16e0c496941ce4d62f247d8b15f560ab
Sergey Bespalov <sergey.bespalov@chisw.us>
2015-11-25 17:02:18 +0200

    add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 /**
  * Created by sergey on 23.11.15.
  */
-var express = require("express");
-var http    = require("http");
-var config  = require("./config");
-var log     = require("./libs/log")(module);
-var route   = require("./route");
+var express     = require("express");
+var http        = require("http");
+var config      = require("./config");
+var log         = require("./libs/log")(module);
+var route       = require("./route");
+var mongoose    = require('mongoose');
 
 var app = express();
 
@@ -17,6 +18,8 @@ http.createServer(app).listen(app.get('port'), function(){
     log.info("Express server listening on port: " + config.get("port"));
 });
 
+
+mongoose.connect('mongodb://localhost/test');
 
 
 
