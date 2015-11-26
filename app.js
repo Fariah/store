bb4d8e001ef9eb26568c1cb136b3dbd4128ed8b3
Sergey Bespalov <sergey.bespalov@chisw.us>
2015-11-26 17:09:13 +0200

    add body parser and save to DB

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 /**
  * Created by sergey on 23.11.15.
  */
-var express     = require("express");
-var http        = require("http");
-var config      = require("./config");
-var log         = require("./lib/log")(module);
-var route       = require("./route");
+var express = require("express");
+var bodyParser = require('body-parser');
+var http = require("http");
+var config = require("./config");
+var log = require("./lib/log")(module);
+var route = require("./route");
 
 var app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.use('/', route);
+
 
 app.set("port", config.get("port"));
 
--- route/index.js
+++ route/index.js
@@ -1,27 +1,62 @@
-var express         = require('express');
-var actions    = require(".././controllers/topicController");
+var express = require('express');
+var Topic = require(".././models/topic").Topic;
 
 var router = express.Router();
 
 
 router.get('/topic', function (req, res) {
-    res.status(200).send(actions.getAll());
+    var topicList = {};
+    Topic.find({}, function(err, topics) {
+        topics.forEach(function(topic) {
+            topicList[topic._id] = topic;
+        });
+
+    });
+
+    res.status(200).send(topicList);
 });
 
 router.get('/topic/:id', function(req, res){
-    res.status(200).send(actions.getTopic(req.params.id));
+
+    console.log('as: ', req.params.id);
+    var topicResult = {};
+
+    Topic.findById(req.params.id, function(err, topic) {
+        console.log("topic: ", topic);
+        console.log("err: ", err);
+        topicResult = topic;
+    });
+
+    console.log("topicResult: ", topicResult);
+
+    res.status(200).send("Get topic: "+topicResult.title);
 });
 
 router.post('/topic', function (req, res) {
-    res.status(201).send(actions.createTopic({"title": "topic_title"}));
+
+    if(req.body.title) {
+        var title = req.body.title;
+    } else {
+        //throw new Error("Неправильный формат данных title");
+        res.status(400).send('Неправильный формат JSON, нужно {"title": "bbbb"}');
+    }
+    var parent = req.body.parent_id ? req.body.parent_id : null;
+
+    var record = new Topic({ title: title, parent_id: parent });
+
+    record.save(function (err, topic, affectef) {
+        if (err) return errorHandler(err);
+    })
+
+    res.status(201).send("Topic " + title + " was created");
 });
 
 router.put('/topic/:id', function (req, res) {
-    res.status(200).send(actions.updateTopic(req.params.id));
+    res.status(200).send("Update topic");
 });
 
 router.delete('/topic/:id', function (req, res) {
-    res.status(200).send(actions.deleteTopic(req.params.id));
+    res.status(200).send("Delete topic");
 });
 
 module.exports = router;
